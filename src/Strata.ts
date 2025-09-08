814a77589487e9be14fbc3fede36620050e6de45
Julian Mclain <julianrmclain@gmail.com>
2025-09-08 09:47:07 -0400

    Update default api host (#3)

--- src/Strata.ts
+++ src/Strata.ts
@@ -111,7 +111,7 @@ export class StrataError extends Error {
   }
 }
 
-const DefaultConnectApiHost = "https://connect.sandbox.connectstrata.com";
+const DefaultConnectApiHost = "https://connect.connectstrata.com";
 const OAuthAuthorizePath = "/oauth/authorize";
 
 /**
