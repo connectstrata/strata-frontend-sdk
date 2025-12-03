c7e7ec148bb28fa192f705057396eec7afa5c7f2
julianmclain <julianrmclain@gmail.com>
2025-12-03 11:49:23 -0500

    Check for project_id env var

--- examples/nextjs/src/app/api/strata-jwt/route.ts
+++ examples/nextjs/src/app/api/strata-jwt/route.ts
@@ -5,17 +5,22 @@ import jwt from "jsonwebtoken";
  * Generates a user JWT token for the Strata SDK
  */
 export async function POST() {
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey) {
+    throw new Error("PRIVATE_KEY is not set");
+  }
+
+  const projectId = process.env.STRATA_PROJECT_ID;
+  if (!projectId) {
+    throw new Error("STRATA_PROJECT_ID is not set");
+  }
+
   const currentTime = Math.floor(Date.now() / 1000);
   const payload: jwt.JwtPayload = {
     sub: "my_user_id",
     project_id: process.env.STRATA_PROJECT_ID,
     iat: currentTime,
   };
-
-  const privateKey = process.env.PRIVATE_KEY;
-  if (!privateKey) {
-    throw new Error("PRIVATE_KEY is not set");
-  }
 
   const token = jwt.sign(payload, privateKey, {
     algorithm: "RS256",
